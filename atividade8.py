aa6c0fb3a83d9c0a3e05876a4dc8efa7ef4789f9
Henry2678 <soldadohuefl@gmail.com>
2024-09-04 15:56:59 -0300

    atividade8

---
+++ atividade8.py
@@ -0,0 +1,4 @@
+# Imagine que você está em uma viagem para os Estados Unidos e precisa converter o valor em reais para dólares. Crie um programa que receba o valor em reais e a taxa de câmbio atual, e exiba o valor equivalente em dólares.
+valor = float(input("digite o valor em reais "))
+conversao = valor*5.63
+print(f"o valor em dolar é {conversao}")
